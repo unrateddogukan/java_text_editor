309c44ca1a2aba5d536491e5def3c3f41f1f86c2
doğukan <80417469+unrateddogukan@users.noreply.github.com>
2021-03-21 16:58:19 +0300

    Delete proje2 directory

--- proje2/Command.java
+++
@@ -1,7 +0,0 @@
-package proje2;
-
-//Programın calismasi icin gerekli komutlarin implement ettigi ve execute metodunu iceren interface.
-public interface Command{
-
-    boolean execute();
-}
--- proje2/UndoableCommand.java
+++
@@ -1,6 +0,0 @@
-package proje2;
-
-//Geri alinabilen komutlarin implement ettigi, Command interface’inden extend edilen ve undo metodunu iceren interface.
-public interface UndoableCommand extends Command{
-    boolean undo();
-}
